6f085a0dcbafaf99d86e1f8702552dbc34f65a33
danielai24 <44975497+danielai24@users.noreply.github.com>
2018-11-12 17:20:57 +0200

    Add files via upload

---
+++ cmmdc2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int a,b;
+int main ()
+{
+    cin>>a;
+    cin>>b;
+    if(a==b){cout<<a;}
+    while(a!=b)
+    {if(a>b){a<--a-b;}
+    else {b<--b-a;}
+    }
+    if(a==1)
+        cout<<"DA";
+    if(b==1)
+        cout<<"NU"
+    return 0;}
