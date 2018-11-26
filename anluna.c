55e927a30f84399f2ee60b8daebc7cf700400af3
danielai24 <44975497+danielai24@users.noreply.github.com>
2018-11-26 17:33:23 +0200

    Add files via upload

---
+++ anluna.c
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+unsigned cod=0;
+int luna;
+int an;
+int zi;
+int main()
+{
+
+}
+        cout<<"Anul";cin>>anul;
+         cout<<"Luna";cin>>luna;
+         cout<<"Ziua";cin>>ziua;
+//operatii pe biti detipul cod=code operator
+//unde elementul e eventual nepus unei deplasari
+      anul=anul<<21;
+      cod<<cod/an;
+       luna=luna<<17;
+       cod=cod/luna
+       cod=cod/ziua;
+      {cout<<"codul datei este"=<<cod;
+}
+
+
+
+
+}
